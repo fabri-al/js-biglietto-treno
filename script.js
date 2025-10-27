e6d30bd27cd9eb471b0cd5915e2c94cf5ec758da
MyUsername <my@email.com>
2025-10-27 15:18:52 +0100

    js-biglietto-treno

---
+++ script.js
@@ -0,0 +1,40 @@
+let etàPasseggiero = prompt("inserisci l'età");
+
+
+
+
+
+while (isNaN(etàPasseggiero)) {
+
+    console.log("devi inserire un numero");
+
+
+    etàPasseggiero = prompt("inserisci l'età");
+
+ console.log("devi inserire un numero");
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
